eb18c737037c4704f23394d19ecb2c2d7513ef75
yanl-pc <70343662+yanlsama@users.noreply.github.com>
2023-04-20 13:50:22 -0500

    proyecto culminado

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const url = 'https://pokeapi.co/api/v2/pokemon/ditto';
 
+document.addEventListener('DOMContentLoaded',()=>{
+  const numId = getRandomInt(1,151);
+  fetchData(numId)
+});
 
 
 // Creando el numero random para la web
@@ -12,19 +15,45 @@ const getRandomInt = (min, max) =>{
 
 // Solicitud a la API
 
-const fetchData = async () =>{
+const fetchData = async (numId) =>{
   try {
-    const res = await fetch(url)
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${numId}`)
     const data = await res.json()
     console.log(data)
+
+    const pokemon = {
+      img: data.sprites.other.dream_world.front_default,
+      nombre : data.name,
+      hp: data.stats[0].base_stat,
+      experiencia: data.base_experience,
+      ataque: data.stats[1].base_stat,
+      especial: data.stats[3].base_stat,
+      defensa: data.stats[2].base_stat
+    }
+
+    pintarCard(pokemon)
   } catch (error) {
     return console.log(error);
   }
 };
 
-fetchData()
 
 
-const print = () =>{
-  
+
+const pintarCard = (pokemon) =>{
+  const main = document.querySelector('.main')
+  const template = document.getElementById('template').content;
+  const clone = template.cloneNode('true');
+  const fragment = document.createDocumentFragment();
+
+  clone.querySelector('.card__photo').setAttribute('src', pokemon.img)
+  clone.querySelector('.card__name').innerHTML = `${pokemon.nombre} <span class="age">${pokemon.hp} hp</span>`
+  clone.querySelector('.card__city').textContent = pokemon.experiencia + ' Exp'
+  clone.querySelectorAll('.card__followers')[0].textContent = pokemon.ataque + 'K'
+  clone.querySelectorAll('.card__followers')[1].textContent = pokemon.especial + 'K'
+  clone.querySelectorAll('.card__followers')[2].textContent = pokemon.defensa + 'K'
+
+  fragment.appendChild(clone)
+  main.appendChild(fragment)
+
 }
